e044c817310c4bcf24f8dc643bf9b3bde20b2a25
Andy <a.kokovikhin@mail.ru>
2022-11-23 20:48:02 +0800

    Add files via upload

---
+++ BackEnd/Models/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Timetable_Backend.Models
+{
+    [Table("Teacher")]
+    public class Teacher
+    {
+        [Key]
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        public int Department_ID { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
