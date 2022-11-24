04096228d3b631e31f98d397412c3e70e90a0f5a
ATsuStudio <103888423+ATsuStudio@users.noreply.github.com>
2022-11-25 00:23:43 +0800

    Add files via upload

---
+++ BackEnd/Models/Models/Official_Leson.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TTB.Models
+{
+    [Table("Official_Leson")]
+    public class Official_Leson
+    {
+        [Key]
+        [Required]
+        public int ID { get; set; }
+
+
+        [Required]
+        public int Teacher_ID { get; set; }
+        [Required]
+        public int Type_ID { get; set; }
+        [Required]
+        public int Audience_ID { get; set; }
+        [Required]
+        public int Day_ID { get; set; }
+        [Required]
+        public int Time_ID { get; set; }
+        [Required]
+        public int Group_ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title_ID { get; set; }
+    }
+}
